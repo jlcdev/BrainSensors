aa64aa5f2494b9e73d64bc81bd783b559d712b06
jlcdev <potnox@gmail.com>
2017-12-18 11:31:53 +0100

    ADD rbind for training sets

--- script.r
+++ script.r
@@ -13,3 +13,12 @@ s3.train3 = read.csv("train_subject3_raw03.asc", header = FALSE, sep="")
 s1.test = read.csv("test_subject1_raw04.asc", header = FALSE, sep="")
 s3.test = read.csv("test_subject3_raw04.asc", header = FALSE, sep="")
 s2.test = read.csv("test_subject2_raw04.asc", header = FALSE, sep="")
+
+s1.train = rbind(s1.train1, s1.train2)
+s1.train = rbind(s1.train, s1.train3)
+
+s2.train = rbind(s2.train1, s2.train2)
+s2.train = rbind(s2.train, s2.train3)
+
+s3.train = rbind(s3.train1, s3.train2)
+s3.train = rbind(s3.train, s3.train3)
