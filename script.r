760e6d1de4f30ebac978a3af21ef533043d61e5c
jlcdev <potnox@gmail.com>
2017-12-18 11:22:20 +0100

    ADD script file with load data in R

---
+++ script.r
@@ -0,0 +1,15 @@
+s1.train1 = read.csv("train_subject1_raw01.asc", header = FALSE, sep="")
+s1.train2 = read.csv("train_subject1_raw02.asc", header = FALSE, sep="")
+s1.train3 = read.csv("train_subject1_raw03.asc", header = FALSE, sep="")
+
+s2.train1 = read.csv("train_subject2_raw01.asc", header = FALSE, sep="")
+s2.train2 = read.csv("train_subject2_raw02.asc", header = FALSE, sep="")
+s2.train3 = read.csv("train_subject2_raw03.asc", header = FALSE, sep="")
+
+s3.train1 = read.csv("train_subject3_raw01.asc", header = FALSE, sep="")
+s3.train2 = read.csv("train_subject3_raw02.asc", header = FALSE, sep="")
+s3.train3 = read.csv("train_subject3_raw03.asc", header = FALSE, sep="")
+
+s1.test = read.csv("test_subject1_raw04.asc", header = FALSE, sep="")
+s3.test = read.csv("test_subject3_raw04.asc", header = FALSE, sep="")
+s2.test = read.csv("test_subject2_raw04.asc", header = FALSE, sep="")
