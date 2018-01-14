a3086fb5515a500b8befcd5fb18de0f01b389a3d
jlcdev <potnox@gmail.com>
2018-01-14 19:26:01 +0100

    CHANGE mlp with caret

--- mlp.r
+++ mlp.r
@@ -1,11 +1,13 @@
 library(dplyr)
 library(MASS)
 library(nnet)
+library(caret)
+library(doParallel)
 
 #MLP
-s1.train1 = read.csv("train_subject1_raw01.asc", header = FALSE, sep="")
-s1.train2 = read.csv("train_subject1_raw02.asc", header = FALSE, sep="")
-s1.train3 = read.csv("train_subject1_raw03.asc", header = FALSE, sep="")
+s1.train1 = read.csv("data/train_subject1_raw01.asc", header = FALSE, sep="")
+s1.train2 = read.csv("data/train_subject1_raw02.asc", header = FALSE, sep="")
+s1.train3 = read.csv("data/train_subject1_raw03.asc", header = FALSE, sep="")
 
 s1.train = rbind(s1.train1, s1.train2)
 s1.train = rbind(s1.train, s1.train3)
@@ -19,15 +21,27 @@ train<-sample_frac(s1.train, 0.7)
 sid<-as.numeric(rownames(train))
 test<-s1.train[-sid,]
 
+decays <- c(0, 0.001, 0.01, 0.1, 0.5)
+trc <- trainControl (method="repeatedcv", number=10, repeats=5)
+
 for (i in 1:32)
 {
   train[,i] <- scale(train[,i])
   test[,i] <- scale(test[,i])
 }
 
-model <- nnet(C ~., data = train, size=10, maxit=3000, decay=0)
-pred.train <- as.factor(predict (model, type = "class"))
-t1 <- table(pred.train,train$C)
+
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
+model <- train(C~., data = train, method = 'nnet', maxit = 100, trace = FALSE, 
+tuneGrid = expand.grid(.size=2, .decay=decays), trControl = trc)
+stopCluster(cl)
+model$bestTune
+model$results
+
+save(model, file="model/mlp_2_0.5_71_S1.model")
+pred.train <- as.factor(predict (model, newdata=test))
+t1 <- table(pred.train,test$C)
 t1
 (error <- 100*(1-sum(t1[row(t1)==col(t1)])/sum(t1)))
 
